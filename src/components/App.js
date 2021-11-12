2d409d85f38997663664f5b110de95f16136f4e4
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2021-11-12 17:53:00 +0000

    initial

---
+++ src/components/App.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import './App.css';
+import Meme from '../abis/Meme.json'
+
+const ipfsApi = require('ipfs-api');
+const ipfs = ipfsApi('ipfs.infura.io', '5001', {protocol: 'https'});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buffer: null,
+      memeHash: 'QmXBjAm5UD8pTdS8PYrCvmCSxWw2eqrMazWSM2vnumv6QC',
+      account: '',
+      contract: null
+    }
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = Meme.networks[networkId]
+    if (networkData) {
+      const contract = web3.eth.Contract(Meme.abi, networkData.address)
+      this.setState({ contract })
+      const memeHash = await contract.methods.get().call()
+      this.setState({ memeHash })
+    } else {
+      window.alert('Smart contract not deployed to detected network!')
+    }
+    console.log(networkId)
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.current.provider)
+    } else {
+      window.alert('Please, use metamask!')
+    }
+  }
+
+  captureFile = (event) => {
+    event.preventDefault();
+    console.log('captured');
+    console.log(event.target.files[0]);
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      console.log('buffer', Buffer(reader.result));
+      this.setState({ buffer: Buffer(reader.result) });
+    }
+  }
+
+  // URL: https://ipfs.infura.io/ipfs/...
+  sendToIpfs = (event) => {
+    event.preventDefault();
+    console.log('sending...');
+    ipfs.add(this.state.buffer,
+             (error, result) => {
+               if (error) {
+                 console.error(error);
+               } else {
+                 const memeHash = result[0].hash;
+                 console.log('hash: ' + memeHash);
+                 this.state.contract.methods.set(memeHash).send({ from: this.state.account })
+                   .then((res) => {
+                     this.setState({ memeHash });
+                   })
+               }
+             }
+    );
+
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.dappuniversity.com/bootcamp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dapp University
+          </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white">{this.state.account}</small>
+            </li>
+          </ul>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`} className="App-logo" alt="logo" />
+                </a>
+                <p></p>
+                <h2>Change meme</h2>
+                <form onSubmit = { this.sendToIpfs }>
+                  <input type='file' onChange = { this.captureFile }/>
+                  <input type='submit'/>
+                </form>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
